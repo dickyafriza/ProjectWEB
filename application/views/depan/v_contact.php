0f5b00c13a5a7c73911bb86f7b2b31a2418ad9ca
Bhakti Raharjo <72753590+bhak-ti@users.noreply.github.com>
2021-07-28 13:29:40 +0700

    update-2

--- application/views/depan/v_contact.php
+++ application/views/depan/v_contact.php
@@ -8,11 +8,11 @@
                     <div class="row">
                     <div class="col-md-12">
                         
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d18819.186197006715!2d109.2417154473421!3d-7.437948564256607!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf876c05f40f089bd!2sMURIS%20STUDIO!5e0!3m2!1sid!2sid!4v1588360407841!5m2!1sid!2sid" frameborder="0" style="width: 100%; height: 100%; position: relative" id="fileframe" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.0432983205209!2d110.3662275872458!3d-6.988870655588305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708adaabcff4af%3A0x764a6ead577fdbf4!2sSekolah%20Dasar%20Nurul%20Islam%20Purwoyoso!5e0!3m2!1sid!2sid!4v1627453666841!5m2!1sid!2sid" frameborder="0" style="width: 100%; height: 100%; position: relative" id="fileframe" allowfullscreen></iframe>
                         
                         <iframe style="width: 100vw;height: 100vh;position: relative;" src="" frameborder="0" allowfullscreen></iframe>
                         
-                    
+                        
                     </div>
                     </div>
                     
