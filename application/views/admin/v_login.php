e1f039188577c54da0ea3d301493c4ee8e97922a
Bhakti Raharjo <72753590+bhak-ti@users.noreply.github.com>
2021-07-29 00:43:47 +0700

    login logo

--- application/views/admin/v_login.php
+++ application/views/admin/v_login.php
@@ -33,7 +33,9 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block">
+                <img src="style/img/logosd.png" width="300px" height="350px">
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
