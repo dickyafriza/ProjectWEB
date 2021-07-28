e7c8a7a89998fa1b667179009bb2911f2df8ba19
afriza <afrizadicky6@gmail.com>
2021-07-28 14:05:34 +0700

    diki3

--- application/views/depan/v_about.php
+++ application/views/depan/v_about.php
@@ -3,7 +3,7 @@
 <div class="event_details_area section__padding">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-14">
                     <div class="single_event d-flex align-items-center">
                         <div class="thumb">
                             <img src="<?php echo base_url('');?>style/img/event/kepala-sekolah.jpg" alt="kepala sekolah">
@@ -15,22 +15,15 @@
                         <div class="event_details_info">
                             <div class="event_info">
                                 <a href="#">
-                                    <h4>SAMBUTAN KEPALA SEKOLAH</h4>
+                                    <h4>PROFILE SEKOLAH</h4>
                                  </a>
-                                <p><span> <i class="flaticon-clock"></i> 10:30 pm</span> <span> <i class="flaticon-calendar"></i> 21 Nov 2020 </span> <span> <i class="flaticon-placeholder"></i> AH Oditoriam</span> </p>
+                                <p><span> <i class="flaticon-clock"></i> 10:30 pm</span> <span> <i class="flaticon-calendar"></i> 28 Juli 2021 </span> <span> <i class="flaticon-placeholder"></i>Semarang</span> </p>
                             </div>
+                            <p style="text-align: justify;"> SD Nurul Islam Purwoyoso merupakan lembaga pendidikan di bawah naungan Yaysan Ta’mir Masjid Nurul Islam Purwoyoso  bersama SMP Nurul Islam dan Taman kanak-kanak Nurul Islam. SD Nurul Islam awal mulanya adalah Madrasah Islamiyah (MI) NU yang didirikan masyarakat sekitar TK Nurul Islam. Dikarenakan para pendiri dari Madrasah Islamiyah (MI) NU tersebut meninggal dunia dan melihat perkembangan TK Nurul Islam, kemudian MI tersebut diserahkan kelembagaan dan pengelolaannya kepada Yayasan Ta’mir Masjid Nurul Islam. Lambat laun MI Nurul Islam berganti nama menjadi SD Nurul Islam dikarenakan pada saat itu masyarakat lebih memilih SD dibandingkan MI.
+                                SD Nurul Islam Purwoyoso berdiri pada tanggal 23 Mei 1996 merupakan salah satu sekolah swasta yang bernuansa islami di kota Semarang. Sekolah ini terletak di jalan Siliwangi No.574 Purwoyoso Kecamatan Ngaliyan Semarang yang dimulai pada tahun ajaran 1996/1997. Sebelum terjadi perluasan wilayah, SD Nurul Islam lebih dikenal dengan SD Nurul Islam Krapyak. Namun setelah terjadi perluasan, kini lebih dikenal dengan SD Nurul Islam.
+                            </p>
                             
-<p>Assalamu'alaikum wr.wb. </p>
-<p>Puji syukur kepada Alloh SWT, Tuhan Yang Maha Esa yang telah memberikan rahmat dan anugerahNya sehingga website SEKOLAH MURIS TEKNOLOGI ini dapat muncul di dunia persilatan (eh salah, didunia pendidikan :P). Melalui layanan internet dan online selama 24 jam, Salah satu tujuan dari website ini adalah untuk menjawab akan setiap kebutuhan informasi dengan memanfaatkan sarana teknologi informasi yang ada. </p>
-<p>Semoga dengan adanya website ini dapat membantu dan bermanfaat, terutama informasi yang berhubungan dengan pendidikan, ilmu pengetahuan dan informasi seputar sekolah. </p> 
-<p>Akhirnya kami mengharapkan masukan dari berbagai pihak untuk website ini agar kami terus belajar dan meng-update diri, sehingga tampilan, isi dan mutu website akan terus berkembang dan lebih baik nantinya. Terima kasih atas kerjasamanya, maju terus untuk mencapai cita-cita yang lebih baik lagi. </p>
-<p>Wassalamu'alaikum wr.wb. </p>
-<p><br>
-</p>
-
-
-                            
-                        </div>
+                       </div>
                     </div>
                 </div>
             </div>
