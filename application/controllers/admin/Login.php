306b443a7990194ef757ff8f42dd7bd570a1f1be
Bhakti Raharjo <72753590+bhak-ti@users.noreply.github.com>
2021-07-29 12:02:31 +0700

    logout fix

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -46,6 +46,6 @@ class Login extends CI_Controller{
 
     function logout(){
         $this->session->sess_destroy();
-        redirect('admin/login');
+        redirect('administrator');
     }
 }
